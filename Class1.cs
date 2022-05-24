7b5b5558d5816a4ac87ae054320d05771a201739
benzatman <benjaminzatman@gmail.com>
2022-05-24 06:20:59 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepsTracker
+{
+    internal class StepsTracker
+    {
+        Dictionary<DateTime, int> stepsDict;
+        int days;
+
+        public StepsTracker()
+        {
+            stepsDict = new Dictionary<DateTime, int>();
+            days = 0;
+        }
+
+        public void addTodaysSteps(int steps)
+        {
+            stepsDict.Add(DateTime.Today, steps);
+            days++;
+            Console.WriteLine("You Have added {0} steps for {1}", steps, DateTime.Today);
+        }
+
+        public int getNumDays()
+        {
+            return days;
+        }
+
+        public int specificDateSteps(DateTime date)
+        {
+            if (stepsDict.ContainsKey(date))
+            {
+                return stepsDict[date];
+            }
+            return -1;
+        }
+        public double lifetimeAveSteps()
+        {
+            int totalSteps = 0;
+            foreach(var step in stepsDict.Values)
+            {
+                totalSteps += step;
+            }
+
+            return (double) totalSteps / days;
+        }
+
+        public double pastWeekAveSteps()
+        {
+            int totalSteps = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                KeyValuePair<DateTime, int> pair = stepsDict.ElementAt(i);
+                totalSteps += pair.Value;
+
+            }
+
+            return (double) totalSteps / 7 ;
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StepsTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StepsTracker st = new StepsTracker();
+            st.addTodaysSteps(13364);
+            double num = st.lifetimeAveSteps();
+            Console.WriteLine("Your have taken on average {0} steps per day since activation", num);
+        }
+    }
+}
